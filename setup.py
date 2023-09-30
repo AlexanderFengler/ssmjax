d5522f21cf7b6bd60401fb689696ff9473148700
AlexanderFengler <alexanderfengler@gmx.de>
2023-09-29 21:19:25 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setuptools
+
+setup(
+    packages=[
+        "ssmjax",
+        "ssmjax.config",
+        "ssmjax.utils",
+        "ssmjax.simulators",
+        "ssmjax.dataset_generators"
+    ],
+)
